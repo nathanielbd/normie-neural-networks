1b19f492e5b966946db5f0b44fe9b52a7c42c98d
Your Name <nathanielbd@gmail.com>
2021-01-06 23:42:26 -0600

    ready to scrape all stats combs

--- scrape.py
+++ scrape.py
@@ -3,33 +3,11 @@ import random
 from selenium.webdriver.support.ui import Select
 import pandas as pd
 
-# driver = webdriver.Firefox(executable_path="/mnt/c/Program Files/Mozilla Firefox/firefox.exe")
-# driver = webdriver.Chrome(executable_path='/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe')
 driver = webdriver.Chrome(executable_path="/mnt/c/Users/nathanielbd/Downloads/chromedriver.exe")
-driver.get('http://dulm.blue/normie')
-values = []
-for i in range(1, 36):
-    to_select = random.choice(driver.find_elements_by_css_selector(f'.question > input[name="q{i}"]'))
-    to_select.click()
-    values.append(to_select.get_attribute('value'))
-for j in range(36, 42):
-    select = Select(driver.find_element_by_id(f'q{j}'))
-    to_select = random.randrange(len(select.options))
-    select.select_by_index(to_select)
-    values.append(select.options[to_select].get_attribute('value'))
-honest = Select(driver.find_element_by_id('honest'))
-honest.select_by_index(1)
-# do the captcha
-idx = int(input('Index of correct captcha: '))
-captcha = driver.find_elements_by_class_name('robotest')[idx-1]
-captcha.click()
-submit_btn = driver.find_element_by_css_selector('[type="submit"]')
-submit_btn.click()
-driver.implicitly_wait(0.5)
-xy = driver.find_elements_by_tag_name('b')[1:3]
-values = values + list(map(lambda x: x.text, xy))
 
-data = pd.DataFrame([values], columns=[
+FILENAME = 'data-stats-combs.csv'
+
+columns = [
     'Do you want to be in society?','Do you make an attempt to socialise?',
     'Do you consume things to alter your perspective? (Alcohol and stuff)',
     'Are you depressed?','Do you try to improve yourself?',
@@ -57,8 +35,16 @@ data = pd.DataFrame([values], columns=[
     'What is your biological sex?','How tall are you?',
     'How attractive are you (from 0 to 10)','What is your BMI?',
     'How many friends and colleagues do you currently have?',
-    'Rate your life right now.','Which climate do you live in?','X','Y'
-])
+    'Rate your life right now.','Which climate do you live in?','X','Y','result'
+]
+
+try:
+    data = pd.read_csv(FILENAME)
+except:
+    data = pd.DataFrame(columns=columns)
+
+def scroll_to(element):
+    driver.execute_script("return arguments[0].scrollIntoView();", element)
 
 from itertools import product
 stats_combs = [
@@ -71,33 +57,60 @@ stats_combs = [
     [0, 1, 2, 3, 4]
 ]
 
-try:
-    for i, comb in enumerate(product(*stats_combs)):
-        # driver.get('http://dulm.blue/normie')
+class CaptchaGoneWrongEvent(Exception):
+    pass
+
+def scrape_comb(k, comb, data, first=False):
+    if first:
+        driver.get('http://dulm.blue/normie')
+        driver.refresh()
+    else:
         driver.back()
-        values = []
-        for i in range(1, 35):
-            to_select = random.choice(driver.find_elements_by_css_selector(f'.question > input[name="q{i}"]'))
-            to_select.click()
-            values.append(to_select.get_attribute('value'))
-        to_select = driver.find_elements_by_css_selector('[name="q35"]')[comb[0]]
+    values = []
+    for i in range(1, 35):
+        to_select = random.choice(driver.find_elements_by_css_selector(f'.question > input[name="q{i}"]'))
+        scroll_to(to_select)
         to_select.click()
-        for j in range(36, 42):
-            select = Select(driver.find_element_by_id(f'q{j}'))
-            to_select = comb[j-35]
-            select.select_by_index(to_select)
-            values.append(select.options[to_select].get_attribute('value'))
-        # idx = int(input('Index of correct captcha: '))
-        # captcha = driver.find_elements_by_class_name('robotest')[idx-1]
-        # captcha.click()
-        honest = Select(driver.find_element_by_id('honest'))
-        honest.select_by_index(1)
-        submit_btn = driver.find_element_by_css_selector('[type="submit"]')
-        submit_btn.click()
-        driver.implicitly_wait(0.5)
-        xy = driver.find_elements_by_tag_name('b')[1:3]
-        values = values + list(map(lambda x: x.text, xy))
-        data.append(values)
-        print(f'added combination {i}')
-finally:
-    data.to_csv('data-stats.csv')
+        values.append(to_select.get_attribute('value'))
+    to_select = driver.find_elements_by_css_selector('[name="q35"]')[comb[0]]
+    values.append(to_select.get_attribute('value'))
+    to_select.click()
+    for j in range(36, 42):
+        select = Select(driver.find_element_by_id(f'q{j}'))
+        to_select = comb[j-35]
+        select.select_by_index(to_select)
+        values.append(select.options[to_select].get_attribute('value'))
+    if first:
+        idx = int(input('Index of correct captcha: '))
+        captcha = driver.find_elements_by_class_name('robotest')[idx-1]
+        captcha.click()
+    honest = Select(driver.find_element_by_id('honest'))
+    honest.select_by_index(1)
+    submit_btn = driver.find_element_by_css_selector('[type="submit"]')
+    submit_btn.click()
+    driver.implicitly_wait(0.5)
+    if driver.find_elements_by_css_selector('.header'):
+        raise CaptchaGoneWrongEvent
+    xy = driver.find_elements_by_tag_name('b')[1:3]
+    result = driver.find_element_by_css_selector('.question > h1')
+    values = values + list(map(lambda x: x.text, xy)) + [result.text]
+    data = data.append(dict(zip(columns, values)), ignore_index=True)
+    print(f'added combination {k}')
+    return False, data
+
+all_combs = list(product(*stats_combs))
+rows_so_far = data.shape[0]
+end_pts = zip(range(rows_so_far, len(all_combs), 600), range(rows_so_far+600, len(all_combs)+600, 600))
+for start, end in end_pts:
+    first = True
+    print(f'Combinations {start} to {end}')
+    for k, comb in enumerate(all_combs[start:end]):
+        try:
+            first, data = scrape_comb(k, comb, data, first)
+        except CaptchaGoneWrongEvent:
+            print('Failed captcha')
+            data.to_csv(FILENAME)
+            driver.close()
+            quit()
+    data.to_csv(FILENAME)
+driver.close()
